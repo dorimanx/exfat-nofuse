12adc053f04499d0d91fd08c34b85ba89e6c295c
rxrz <rxrzlvzu@shortmail.me>
2013-07-23 04:31:03 +0000

    Update exfat_version.h

--- exfat_version.h
+++ exfat_version.h
@@ -4,6 +4,17 @@
 /*  FILE    : exfat_version.h                                           */
 /*  PURPOSE : exFAT File Manager                                        */
 /*                                                                      */
+/*----------------------------------------------------------------------*/
+/*  NOTES                                                               */
+/*                                                                      */
+/*----------------------------------------------------------------------*/
+/*  REVISION HISTORY                                                    */
+/*                                                                      */
+/*  - 2012.02.10 : Release Version 1.1.0                                */
+/*  - 2012.04.02 : P1 : Change Module License to Samsung Proprietary    */
+/*  - 2012.06.07 : P2 : Fixed incorrect filename problem                */
+/*  - 2013.07.22 : 1.1.4 : Change Module License to Samsung Proprietary */
+/*                         (once again, this time for sure)             */
 /************************************************************************/
 
-#define EXFAT_VERSION  "1.1.3"	
+#define EXFAT_VERSION  "1.1.4"
