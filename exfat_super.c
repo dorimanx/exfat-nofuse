b0ccf5788e8b618cdc42109c502ff90cd1ac0b65
Dorimanx <yuri@bynet.co.il>
2013-09-08 12:01:48 +0300

    Fixed Build error for < 3.7.y tree. TNX @twu2

--- exfat_super.c
+++ exfat_super.c
@@ -1713,7 +1713,10 @@ static void exfat_free_super(struct exfat_sb_info *sbi)
 		unload_nls(sbi->nls_io);
 	if (sbi->options.iocharset != exfat_default_iocharset)
 		kfree(sbi->options.iocharset);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,7,0)
+	/* mutex_init is in exfat_fill_super function. only for 3.7+ */
 	mutex_destroy(&sbi->s_lock);
+#endif
 	kfree(sbi);
 }
 
